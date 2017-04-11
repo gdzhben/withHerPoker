43ddf391599670763272ad46821174d4bdca3bcc
darryyylh <darrylhennessy95@gmail.com>
2017-04-11 19:33:57 +0100

    classification progress

--- src/pokergame/HandClassifier.ts
+++ src/pokergame/HandClassifier.ts
@@ -4,6 +4,21 @@ import { IHandClassifier } from './IHandClassifier';
 
 export class HandClassifier implements IHandClassifier {
 
-    
+    private _cards: Card[];
+
+    constructor(hand: Hand) {
+        if (hand.size() != 5) 
+            throw new Error("Only a full hand can be classified");
+
+        this._cards = [];
+
+        for (let i = 0; i < hand.size(); i++) {
+            this._cards[i] = hand.getCard(i);
+        }
+    }
+
+    public getHandType(): HandType {
+        return null;
+    }
 
 }
--- src/pokergame/IHandClassifier.ts
+++ src/pokergame/IHandClassifier.ts
@@ -1,5 +1,7 @@
-
+import { HandType } from '../types/HandType';
 
 export interface IHandClassifier {
+
+    getHandType(): HandType;
 
 }
