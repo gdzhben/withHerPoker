30872e5bec9dd9e6ce825ba963cace431d7b2084
Ben Ning <gdzhben@hotmail.com>
2017-04-11 19:13:21 +0100

    Ben (#8)
    * test for deck added, card comparesion method added in deck class
    
    * Fix according to review
    
    * Fixed
    
    * Fix accordingly

---
+++ src/pokergame/IDeck.ts
@@ -0,0 +1,11 @@
+import { SuitType } from '../types/SuitType';
+import { ICard } from './ICard'
+
+
+export interface IDeck {
+    shuffle(): void;
+    returnCard(Card: ICard): void;
+    drawCard(): ICard;
+    sizeOfDeck(): number;
+    containCard(card: ICard): boolean;
+}
