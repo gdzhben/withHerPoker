cd88543072d184154744b504b1a992ac7b94e57d
12252511 <zhesi.ning@ucdconnect.ie>
2017-04-12 15:24:46 +0100

    fixed conflict

--- src/pokergame/Card.ts
+++ src/pokergame/Card.ts
@@ -3,15 +3,10 @@ import { ICard } from './ICard';
 
 export class Card implements ICard {
 
-<<<<<<< HEAD
+
     private _cardName: string;
     private _suitType: SuitType;
     private _faceValue: number;
-=======
-    private _cardName: string
-    private _suitType: SuitType
-    private _faceValue: number
->>>>>>> 15b01ef9b54a13dd2e2e11c94f211013b9cc4e26
 
     constructor(cardName: string, suitType: SuitType, faceValue: number) {
         this._cardName = cardName;
@@ -20,34 +15,25 @@ export class Card implements ICard {
     }
 
     public getCardName(): string {
-<<<<<<< HEAD
-        return this._cardName;
-    }
-
-=======
 
         return this._cardName;
     }
 
->>>>>>> 15b01ef9b54a13dd2e2e11c94f211013b9cc4e26
     public getSuitType(): SuitType {
         return this._suitType;
     }
 
     public getFaceValue(): number {
         return this._faceValue;
-<<<<<<< HEAD
-=======
-
 
     }
 
     public getSuitTypeString(): string {
-        if (this._suitType == SuitType.CLUBS) 
+        if (this._suitType == SuitType.CLUBS)
             return "Clubs";
-        else if (this._suitType == SuitType.DIAMONDS) 
+        else if (this._suitType == SuitType.DIAMONDS)
             return "Diamonds";
-        else if (this._suitType == SuitType.HEARTS) 
+        else if (this._suitType == SuitType.HEARTS)
             return "Hearts";
         else
             return "Spades";
@@ -57,7 +43,6 @@ export class Card implements ICard {
         let str = this.getCardName() + this.getSuitTypeString();
         return str;
 
->>>>>>> 15b01ef9b54a13dd2e2e11c94f211013b9cc4e26
-    }
 
+    }
 }
