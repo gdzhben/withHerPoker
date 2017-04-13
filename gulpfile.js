7bb075d1fb701998df411061d1fa3e915b4ac00a
Sukrat Kashyap (Sukhi) <sukraat021195@gmail.com>
2017-04-13 17:35:42 +0100

    Added code coverage (#22)

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
 const clean = require('gulp-clean');
 const jasmineBrowser = require('gulp-jasmine-browser');
-
+const istanbul = require('gulp-istanbul');
+const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+var open = require('gulp-open');
 
 const paths = {
     build: {
@@ -18,6 +20,13 @@ const paths = {
         jsFiles: 'src/**/*.js',
         specJsFiles: 'src/**/*.spec.js',
         all: 'src/**/*'
+    },
+    coverage: {
+        root: 'coverage',
+        jsonFile: 'coverage/coverage-final.json',
+        mapJson: 'coverage/coverage-map.json',
+        mapHtmlReport: 'coverage/html-report',
+        indexFile: 'coverage/html-report/index.html'
     }
 }
 
@@ -36,4 +45,36 @@ gulp.task('cleanjs', function () {
         paths.build.jsFiles,
         paths.build.jsMapFiles], { read: false })
         .pipe(clean());
+});
+
+gulp.task('clean-coverage', function () {
+    return gulp.src(paths.coverage.root, { read: false })
+        .pipe(clean());
+});
+
+gulp.task('pre-coverage', ['clean-coverage'], function () {
+    return gulp.src([paths.build.jsFiles, '!' + paths.build.specJsFiles])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('covering', ['pre-coverage'], function () {
+    return gulp.src(paths.build.specJsFiles)
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports());
+});
+
+gulp.task('coverage', ['covering'], function () {
+    return gulp.src(paths.coverage.jsonFile)
+        .pipe(remapIstanbul({
+            reports: {
+                'json': paths.coverage.mapJson,
+                'html': paths.coverage.mapHtmlReport
+            }
+        }));
+});
+
+gulp.task('open-coverage-report', function () {
+    gulp.src(paths.coverage.indexFile)
+        .pipe(open());
 });
