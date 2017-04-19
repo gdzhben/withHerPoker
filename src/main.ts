6e92ac925809e1b770e7b4df6b17801250de0b91
Sukrat Kashyap (Sukhi) <sukraat021195@gmail.com>
2017-04-19 15:11:11 +0100

    Added Leaderboard api (#30)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { App } from './App';
+import * as ex from 'express';
+import { MongoDb } from './mongodb/MongoDb';
 
 var app = new App();
 app.start();
+
+var express: ex.Application = ex();
+
+express.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+
+let testConnectionString = 'mongodb://withHer:1234@localhost/test';
+var mongo = new MongoDb(testConnectionString);
+express.get('/leaderboard', (req, res) => {
+    let data = mongo.getLeaderboard(10);
+    res.send(data);
+});
