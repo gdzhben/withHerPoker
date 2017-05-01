357eb145e11ac93dc5da55ab9f584ca046ec9e8c
Ben Ning <gdzhben@hotmail.com>
2017-05-01 02:28:53 +0100

    added some messenge in messenge class (#36)

---
+++ src/interfaces/messenge.ts
@@ -0,0 +1,7 @@
+export class messenge {
+    public readonly _WELCOME_MESSAGE: string = "Warm welcome to playing poker with her. ";
+    public readonly _INSTRUCTION_MESSAGE: string = "You will be playing this poker game with 4 other bot player. \n At the start of the game you will all be given amount of 100 game chips to bet. In the first round you will have the option to either FOLD, SEE or RAISE. then you can choose to weather you want to discard some of your cards.\n The maximum number of cards you can discard is 3.\n Then in the second round you can again choose to  FOLD, SEE or RAISE. following by 3rd round, everyone have to show their cards and the game will announce the winner. Then move to next round.";
+    public readonly _INSTRUCTION_MESSAGE_2:string="In the case of a bot player runing out of poker chips it will not able to make to the next round. remaining player will containue the game to next round. But if the human player runing out of chips. Game will over."
+    public readonly _ERROR_COMMAND_MESSAGE:string="Your input to the game was invalid. your input should be command as following: SEE RAISE FOLD or DISCARD ";
+
+}
